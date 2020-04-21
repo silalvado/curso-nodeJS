ae57ffc2c9d55c9e5f19f2995ae4c933ec1b3022
silalvado <silvialvado@gmail.com>
2020-04-21 11:32:13 -0300

    Renamed variables

--- unsuccessTransactions.js
+++ unsuccessTransactions.js
@@ -4,13 +4,13 @@ fs.readFile('transactions.txt', 'utf-8', (error,data) => {
     if (error) {
         throw error;
     }
-    let lines = data.split('\n');
-    for (i = 0; i < lines.length; i++){
-        let item = lines[i].split(':');
-        if (item.length >= 4) {
-            result = item[3].toUpperCase();
+    let transactions = data.split('\n');
+    for (i = 0; i < transactions.length; i++){
+        let transactionItems = transactions[i].split(':');
+        if (transactionItems.length >= 4) {
+            result = transactionItems[3].toUpperCase();
             if (result == 'RJ' || result == 'CD'){
-                console.log (item[0]);
+                console.log (transactionItems[0]);
             }
         }
     }
